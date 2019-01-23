4d363b973d40602f5d1c5f310e77c29d4448ff32
srinivasanrjgpl2010@gmail.com <srinivasanrjgpl2010@gmail.com>
2019-01-23 20:19:38 +0530

    Initial commit for project structure setup

---
+++ setup.py
@@ -0,0 +1,10 @@
+from setuptools import find_packages, setup
+
+setup(
+    name='src',
+    packages=find_packages(),
+    version='0.1.0',
+    description='Repo for Pluralsight intro to data science course',
+    author='Srinivasan K R',
+    license='MIT',
+)
